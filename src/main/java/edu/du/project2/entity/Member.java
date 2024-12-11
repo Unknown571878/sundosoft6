d37fe009291d617049538a34c6e227a626d8afd1
kimgeonjung <rjs2021@gmail.com>
2024-12-11 18:13:27 +0900

    test2

---
+++ src/main/java/edu/du/project2/entity/Member.java
@@ -0,0 +1,28 @@
+package edu.du.project2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String password;
+    private String email;
+    private String address;
+    private String detailAdd;
+    private String tel;
+    private String role;
+}
