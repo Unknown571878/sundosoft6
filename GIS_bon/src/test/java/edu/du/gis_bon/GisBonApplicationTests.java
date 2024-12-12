2c6089d375bd5f48ab10db7e291261533840fb6c
phj-reckless <phjhj6181@naver.com>
2024-12-12 17:45:09 +0900

    gis

---
+++ GIS_bon/src/test/java/edu/du/gis_bon/GisBonApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.du.gis_bon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisBonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
