15d91399d5de74348459c7ff74411dc91c39fe54
seoung342 <seoung342@gmail.com>
2024-12-16 17:13:25 +0900

    test1

--- src/main/java/edu/du/project2/Project2Application.java
+++ src/main/java/edu/du/project2/Project2Application.java
@@ -1,17 +1,25 @@
 package edu.du.project2;
 
 import edu.du.project2.dto.MemberRequest;
+import edu.du.project2.entity.Notice;
+import edu.du.project2.entity.QnA;
+import edu.du.project2.entity.QnAList;
 import edu.du.project2.service.MemberService;
+import edu.du.project2.service.NoticeService;
+import edu.du.project2.service.QnAService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @SpringBootApplication
 public class Project2Application {
 
     private final MemberService memberService;
+    private final NoticeService noticeService;
+    private final QnAService qnAService;
     public static void main(String[] args) {
         SpringApplication.run(Project2Application.class, args);
     }
@@ -27,9 +35,9 @@ public class Project2Application {
         memberRequest.setDetailAddress("");
         memberService.registerMember(memberRequest);
 
+        Notice notice = new Notice();
+        notice.setContent("오늘 하루가 길다.");
+        notice.setTitle("오늘 하루가 길다");
+        noticeService.createNotice(notice.getTitle(), notice.getContent());
     }
-
-
-
-
 }
