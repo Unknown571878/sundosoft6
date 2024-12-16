7562448349c1cd8097abd367b900fd5470288943
kimgeonjung <rjs2021@gmail.com>
2024-12-16 16:38:24 +0900

    회원가입 수정중

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -2,10 +2,14 @@ $(document).ready(function (){
     const emailInput = $('#email');
     const codeInput = $('#code');
     const hiddenEmailField = $('#hiddenEmail');
+    const emailBtn = $('#email-btn');
+    const codeBtn = $('#code-btn');
 
     // 이메일 인증번호 발송
     $('#checkEmail').on('submit', function (event) {
         event.preventDefault();
+        emailBtn.prop("disabled", true)
+                .text("인증 진행 중...");
 
         $.ajax({
             url: '/api/mail/emailCheck',
@@ -20,6 +24,12 @@ $(document).ready(function (){
             error: function (xhr) {
                 alert('이메일 인증 번호 전송 실패');
                 console.error(xhr.responseText);
+                emailInput.prop('readonly', false);
+                emailBtn.prop("disabled", false).text("이메일 인증");
+                emailBtn.removeClass('btn-outline-primary').addClass('btn-outline-success');
+                emailBtn.text("인증 완료");
+                codeBtn.removeClass('btn-outline-primary').addClass('btn-outline-success');
+                codeBtn.text("인증 완료");
             }
         });
     });
@@ -39,9 +49,13 @@ $(document).ready(function (){
                 codeInput.prop('readonly', true);
                 hiddenEmailField.val(email);
                 console.log('Hidden email field value:', hiddenEmailField.val());
+                emailBtn.prop('disabled', true).text('인증 성공');
             },
             error: function (error) {
                 alert('인증 실패. 잘못된 인증번호입니다.');
+                codeInput.val('');
+                emailBtn.prop("disabled", false).text("이메일 인증");
+                emailInput.prop('readonly', false).focus();
             }
         });
     });
