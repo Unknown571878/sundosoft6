efe3e3ccaff750031233a2e688f17304ae5a1fe7
Unknown571878 <yyt365t@naver.com>
2024-12-16 17:08:14 +0900

    FAQ 추가

---
+++ src/main/java/edu/du/project2/controller/FAQController.java
@@ -0,0 +1,38 @@
+package edu.du.project2.controller;
+
+import edu.du.project2.entity.FaQ;
+import edu.du.project2.service.FAQService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/faq")
+public class FAQController {
+
+    private final FAQService faqService;
+
+    @GetMapping("")
+    public String faq(Model model, @PageableDefault(page = 0, size = 10) Pageable pageable) {
+        Page<FaQ> lists = faqService.getFAQs(pageable);
+        model.addAttribute("faqs", lists);
+        return "/faq/faqList";
+    }
+
+    @GetMapping("/faqDetail")
+    public String faqDetail(@RequestParam("id") Long id, Model model) {
+        FaQ faQ = faqService.faqDetail(id);
+        model.addAttribute("faq", faQ);
+        return "/faq/faqDetail";
+    }
+}
