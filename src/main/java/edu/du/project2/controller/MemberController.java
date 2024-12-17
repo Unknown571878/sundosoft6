c88a72c9c00acef365029a44291cd4dd6c48f55e
kimgeonjung <118881591+kimgeonjung@users.noreply.github.com>
2024-12-17 18:46:30 +0900

    Update MemberController.java

--- src/main/java/edu/du/project2/controller/MemberController.java
+++ src/main/java/edu/du/project2/controller/MemberController.java
@@ -88,14 +88,14 @@ public class MemberController {
         return "redirect:/login";  // 로그인 페이지로 리디렉션
     }
 
-    @GetMapping("/idfor")
+    @GetMapping("/findId")
     public String idforPage() {
-        return "user/idfor";  // idfor.html로 이동
+        return "user/findId";  // idfor.html로 이동
     }
 
-    @GetMapping("/passwordfor")
+    @GetMapping("/findPw")
     public String passwordforPage() {
-        return "user/passwordfor";  // passwordfor.html로 이동
+        return "user/findPw";  // passwordfor.html로 이동
     }
 
 }
