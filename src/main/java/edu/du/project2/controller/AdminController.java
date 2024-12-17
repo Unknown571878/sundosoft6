cc47e6bc398deb557c4ed9c36da7c37e1c5be3f4
seoung342 <seoung342@gmail.com>
2024-12-17 17:30:30 +0900

    2

--- src/main/java/edu/du/project2/controller/AdminController.java
+++ src/main/java/edu/du/project2/controller/AdminController.java
@@ -16,6 +16,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -145,4 +148,25 @@ public class AdminController {
         return "redirect:/admin_notice";
     }
 
+
+
+
+
+
+
+    @GetMapping("/admin_management")
+    public String management(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "admin/admin_management";
+    }
+
+    @PostMapping("/admin/deleteMember")
+    public ResponseEntity<String> deleteMember(@RequestBody Map<String, Object> request) {
+        Long memberId = Long.valueOf(request.get("id").toString());
+        memberRepository.deleteById(memberId);
+
+
+        return ResponseEntity.ok("삭제 성공");
+    }
 }
