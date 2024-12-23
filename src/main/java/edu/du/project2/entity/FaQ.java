d054fff66119e48508f131cb15955dd395891cb3
seoung342 <seoung342@gmail.com>
2024-12-23 16:52:25 +0900

    1

--- src/main/java/edu/du/project2/entity/FaQ.java
+++ src/main/java/edu/du/project2/entity/FaQ.java
@@ -7,10 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
