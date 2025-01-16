98f314df8af6841b6344f6bee209d2dc8a1537fc
Unknown571878 <yyt365t@naver.com>
2025-01-16 10:05:54 +0900

    입지분석 버튼 수정

--- src/main/java/edu/du/project2/config/WebConfig.java
+++ src/main/java/edu/du/project2/config/WebConfig.java
@@ -33,6 +33,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(userInterceptor).addPathPatterns("/admin/**", "/admin", "/qna/**", "/analysis/**", "/profile/**", "/api/ai", "/download/**");
+        registry.addInterceptor(userInterceptor).addPathPatterns("/admin/**", "/admin", "/qna/**", "/analysis/**", "/profile/**", "/download/**");
     }
 }
